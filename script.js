bf1b9bd0f804b2802d64428d5a7680164bbc7169
Vyacheslav Tkachenko <vyacheslavtkachenko@Vyacheslavs-MacBook-Pro.local>
2021-01-20 11:06:21 +0200

    add script file

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log();
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели ?");
+const personalMovieDB = {
+    count:'',
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+ personalMovieDB.count = numberOfFilms;  
+for(let i = 0; i<=1;i++){
+    let lastWatchFilms = prompt("Один из последних просмотреных фильмов ?");
+    let appraisal = prompt('На сколько его оцените ?');
+    personalMovieDB.movies[lastWatchFilms]=appraisal;
+}
+console.log(personalMovieDB);
